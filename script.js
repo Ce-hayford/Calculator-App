265694923f450febd09326aab3ed6872d1106609
NeonHayford <hayford.lumorvi@gmail.com>
2023-03-13 11:59:09 +0000

    commit

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let valueContainer = document.getElementById('btn-Numbers');
-console.log(valueContainer);
+// console.log(valueContainer);
+
 let appendDiv = '';
 // valueContainer.innerHTML = '<>'
-for(let i=1; i <= 4; i++){
+for(let i=1; i < 5; i++){
     appendDiv += 
     '<div class="btn">'+[i]+'</div>';
     valueContainer.innerHTML = appendDiv;
@@ -10,33 +11,30 @@ for(let i=1; i <= 4; i++){
 let cut = document.getElementsByClassName('btn');
 let newCut = Array.from(cut);
 newCut[3].innerHTML = 'Del';
-console.warn(newCut[3]);
+// console.warn(newCut[3]);
 
-for(let j=4; j <= 8; j++){
+for(let j=4; j < 8; j++){
     appendDiv += 
     '<div class="btn">'+[j]+'</div>';
     valueContainer.innerHTML = appendDiv;
 }
 let dart= document.getElementsByClassName('btn');
 let newdart = Array.from(dart);
-newdart[8].innerHTML = '+';
-console.warn(newdart[3]);
-for(let k=8; k <=12; k++){
+newdart[7].innerHTML = '+';
+// console.warn(newdart[7]);
+
+for(let k=7; k < 11; k++){
     appendDiv += 
     '<div class="btn">'+[k]+'</div>';
     valueContainer.innerHTML = appendDiv;
 }
 let ert = document.getElementsByClassName('btn');
 let newErt = Array.from(ert);
-newErt[12].innerHTML = '-';
-console.warn(newErt[12]);
+newErt[11].innerHTML = '-';
+// console.warn(newErt[11]);
 
-for(let l=12; l <= 16; l++){
-    appendDiv += 
-    '<div class="btn">'+[j]+'</div>';
-    valueContainer.innerHTML = appendDiv;
-}
-let qurt= document.getElementsByClassName('btn');
-let newqurt = Array.from(qurt);
-newqurt[16].innerHTML = '*';
-console.warn(newqurt[16]);
+let SubmitBtnContainer = document.querySelector('.submit-btn');
+SubmitBtnContainer.innerHTML=`<input type="reset" class="resetBtn" value="reset" /> <input type="button" class="submitBtn" value="=" />`;
+ 
+console.group(SubmitBtnContainer);
+console.log(SubmitBtnContainer);
